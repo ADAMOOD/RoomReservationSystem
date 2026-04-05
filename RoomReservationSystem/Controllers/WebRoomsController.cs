8769221464e3cda7b1d2ac2b955c19c0f9f996f5
ADAMOOD <99761968+ADAMOOD@users.noreply.github.com>
2026-04-05 12:50:13 +0200

    reservation invalid inputs fixed Commit before reservation UI and UX update

--- RoomReservationSystem/Controllers/WebRoomsController.cs
+++ RoomReservationSystem/Controllers/WebRoomsController.cs
@@ -38,10 +38,36 @@ namespace RoomReservationSystem.Controllers
             CreateReservationViewModel newReservation = new CreateReservationViewModel()
             {
                 RoomId = id,
-                RoomName = room.Name
+                RoomName = room.Name,
+                RoomCapacity = room.Capacity,
+                MaxReservationMinutes = room.MaxReservationMinutes
             };
             return View(newReservation);
         }
+        [HttpGet]
+        public async Task<IActionResult> GetRoomEvents(int roomId)
+        {
+            var reservations = await _reservationRepository.GetAllReservationsForRoomAsync(roomId);
+
+            var calendarEvents = reservations.Select(r => new
+            {
+                title = r.Purpose,
+                start = r.StartTime.ToString("yyyy-MM-ddTHH:mm:ss"), // Important: ISO 8601 formát času
+                end = r.EndTime.ToString("yyyy-MM-ddTHH:mm:ss"),
+                color = "#dc3545",
+                allDay = false // times not all days
+            });
+
+            return Json(calendarEvents);
+        }
+        //TEST
+        [HttpGet]
+        public IActionResult CalendarTest(int roomId)
+        {
+            ViewBag.RoomId = roomId; // Tímto jednoduše pošleme ID do HTML
+            return View();
+        }
+        //TEST
 
         [Authorize]
         [HttpPost]
@@ -51,11 +77,23 @@ namespace RoomReservationSystem.Controllers
             {
                 return View(model);
             }
+            var existingReservations = await _reservationRepository.GetAllReservationsForRoomAsync(model.RoomId);
+
+            bool isOverlapping = existingReservations.Any(r =>
+                r.StartTime < model.EndTime.Value &&
+                r.EndTime > model.StartTime.Value);
+
+            if (isOverlapping)
+            {
+                ModelState.AddModelError("", "Sorry, but there is already a reservation at this time");
+                return View(model);
+            }
+
             int loggedUserId = int.Parse(User.FindFirstValue("UserId"));//user is a shortcut for HttpContext.User which we have access due to the Controller inharitance
             Reservation newReservation = new Reservation()
             {
                 RoomId = model.RoomId, 
-                StartTime = model.StartTime.Value, // value becose dates are nullable
+                StartTime = model.StartTime.Value, // value because dates are nullable
                 EndTime = model.EndTime.Value,
                 OrganizerId = loggedUserId,
                 PersonCount = model.PersonCount,
@@ -64,8 +102,7 @@ namespace RoomReservationSystem.Controllers
             };
             await _reservationRepository.CreateReservationAsync(newReservation);
             return RedirectToAction("Index");
-
-
         }
+
     }
 }
--- RoomReservationSystem/ViewModels/CreateReservationViewModel.cs
+++ RoomReservationSystem/ViewModels/CreateReservationViewModel.cs
@@ -2,7 +2,7 @@
 
 namespace RoomReservationSystem.ViewModels
 {
-    public class CreateReservationViewModel
+    public class CreateReservationViewModel : IValidatableObject
     {
         public int RoomId { get; set; }
         public string RoomName { get; set; } = string.Empty;
@@ -15,6 +15,8 @@ namespace RoomReservationSystem.ViewModels
 
         public string Purpose { get; set; } = string.Empty;
         public int PersonCount { get; set; } = 1;
+        public int RoomCapacity { get; set; }
+        public int MaxReservationMinutes { get; set; } 
         //automaticly called when form is send
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
@@ -26,6 +28,20 @@ namespace RoomReservationSystem.ViewModels
             if (StartTime.HasValue && EndTime.HasValue && EndTime.Value <= StartTime.Value)
             {
                 yield return new ValidationResult("End time of the reservation can not be set prior to the start date.", new[] { nameof(EndTime) });
+            }
+            
+            if (PersonCount > RoomCapacity)
+            {
+                yield return new ValidationResult($"Room capacity is only {RoomCapacity} people.", new[] { nameof(PersonCount) });
+            }
+
+            if (StartTime.HasValue && EndTime.HasValue)
+            {
+                var duration = EndTime.Value - StartTime.Value;
+                if (duration.TotalMinutes > MaxReservationMinutes)
+                {
+                    yield return new ValidationResult($"Reservation time cannot be greather than {MaxReservationMinutes} minutes.", new[] { nameof(EndTime) });
+                }
             }
         }
     }
