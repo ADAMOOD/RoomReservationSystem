c9ed4002d0a61803f3c0167326320e5531218f6e
ADAMOOD <99761968+ADAMOOD@users.noreply.github.com>
2026-04-19 17:39:51 +0200

    desktomApp Setup

---
+++ RoomReservationSystem.Desktop/Services/ApiService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+using System.Net.Http.Headers;
+
+namespace RoomReservationSystem.Desktop.Services
+{
+    public class ApiService
+    {
+        private readonly HttpClient _httpClient;
+        private string? Token { get; set; }
+        public HttpClient Client
+        {
+            get { return _httpClient; }
+        }
+
+        public ApiService()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:7266/");
+        }
+
+        public void SetToken(string token)
+        {
+            Token = token;
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+        }
+    }
+}
